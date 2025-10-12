e111bc454543ecf695918f541203fb70c2b9b698
Nadeeshana-Lahiru <nadeeshana1998@gmail.com>
2025-10-12 15:02:56 +0530

    LED and BUZZER with push button

---
+++ PlatformIO-with-WOKWI-Simulation/LED and BUZZER with Push Button/src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+int led_pin = 12;
+int btn_pin = 13;
+int buzzer_pin = 11;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led_pin, OUTPUT);
+  pinMode(btn_pin, INPUT_PULLUP);
+  pinMode(buzzer_pin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int btn_state = digitalRead(btn_pin);
+
+  if(btn_state == LOW){
+    digitalWrite(led_pin, HIGH);
+    tone(buzzer_pin,HIGH);
+  }
+  else if(btn_state == HIGH){
+    digitalWrite(led_pin, LOW);
+    noTone(led_pin);
+  }
+}
+
